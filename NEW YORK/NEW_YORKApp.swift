53c5e78eebc2fe9a4610026a135ba5b6b07048a8
Max <dooksiep33@gmail.com>
2024-12-18 12:28:53 -0500

    Initial Commit

---
+++ NEW YORK/NEW_YORKApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEW_YORKApp.swift
+//  NEW YORK
+//
+//  Created by Maxwell Powell (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NEW_YORKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
