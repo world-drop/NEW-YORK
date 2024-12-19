5d0f5a4c87c0ae97724cc04c597c6bead74cdffb
Max <dooksiep33@gmail.com>
2024-12-19 13:42:38 -0500

    web

---
+++ NEW YORK/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  NEW YORK
+//
+//  Created by Maxwell Powell (student LM) on 12/19/24.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
